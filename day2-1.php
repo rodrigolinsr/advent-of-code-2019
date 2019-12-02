e3d086e388c7c6359312208f1dcd1a390eb58fae
Rodrigo Lins Rodrigues <rodrigolinsr@gmail.com>
2019-12-02 19:51:05 +1300

    Day 2

---
+++ day2-1.php
@@ -0,0 +1,52 @@
+<?php
+function calc(array &$inputsArray)
+{
+    for ($i = 0; isset($inputsArray[($i + 4)]); $i += 4) {
+        $opcode = intval($inputsArray[$i]);
+
+        if ($opcode === 99 || !in_array($opcode, [1,2,99])) {
+            break;
+        }
+
+        $index1      = $inputsArray[$i+1];
+        $index2      = $inputsArray[$i+2];
+        $indexTarget = $inputsArray[$i+3];
+
+        $value1 = $inputsArray[$index1];
+        $value2 = $inputsArray[$index2];
+
+        if ($opcode === 1) {
+            $inputsArray[$indexTarget] = $value1 + $value2;
+        }
+
+        if ($opcode === 2) {
+            $inputsArray[$indexTarget] = $value1 * $value2;
+        }
+    }
+}
+
+$example = [1,0,0,0,99];
+calc($example);
+var_dump($example);
+
+$example = [2,3,0,3,99];
+calc($example);
+var_dump($example);
+
+$example = [2,4,4,5,99,0];
+calc($example);
+var_dump($example);
+
+$example = [1,1,1,4,99,5,6,0,99];
+calc($example);
+var_dump($example);
+
+$inputs = trim(file_get_contents('/Users/rodrigo/Development/advent-of-code-2019/inputs/day2.txt'));
+
+$inputsArray = explode(',', $inputs);
+$inputsArray[1] = 12;
+$inputsArray[2] = 2;
+
+calc($inputsArray);
+
+var_dump($inputsArray[0]);
---
+++ day2-2.php
@@ -0,0 +1,49 @@
+<?php
+function calc(array &$inputsArray)
+{
+    for ($i = 0; isset($inputsArray[($i + 4)]); $i += 4) {
+        $opcode = intval($inputsArray[$i]);
+
+        if ($opcode === 99 || !in_array($opcode, [1,2,99])) {
+            break;
+        }
+
+        $index1      = $inputsArray[$i+1];
+        $index2      = $inputsArray[$i+2];
+        $indexTarget = $inputsArray[$i+3];
+
+        $value1 = $inputsArray[$index1];
+        $value2 = $inputsArray[$index2];
+
+        if ($opcode === 1) {
+            $inputsArray[$indexTarget] = $value1 + $value2;
+        }
+
+        if ($opcode === 2) {
+            $inputsArray[$indexTarget] = $value1 * $value2;
+        }
+    }
+}
+
+
+$originalInputs = trim(file_get_contents('/Users/rodrigo/Development/advent-of-code-2019/inputs/day2.txt'));
+
+$limit = 100;
+for ($noun = 0; $noun <= $limit; $noun++) {
+    for($verb = 0; $verb <= $limit; $verb++) {
+        $inputsArray = explode(',', $originalInputs);
+        $inputsArray[1] = $noun;
+        $inputsArray[2] = $verb;
+
+        calc($inputsArray);
+
+        if ($inputsArray[0] == 19690720) {
+            var_dump($noun);
+            var_dump($verb);
+            var_dump("The answer is: " . (100 * $noun + $verb));
+            exit;
+        }
+    }
+}
+
+var_dump("nothing found :( increase the limit ($limit)");
